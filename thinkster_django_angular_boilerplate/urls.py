1b4c395e516fdad5ca4b771a9ecd5d4dd70c0119
Vadtec <vadtec@vadtec.net>
2015-01-12 17:39:22 +0200

    authentication. iss: METOD_NOT_ALLOWED

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,9 +1,18 @@
-from django.conf.urls import patterns, url
+# .. Imports
+from rest_framework_nested import routers
 
-from thinkster_django_angular_boilerplate.views import IndexView
+from views import IndexView
+from authentication.views import AccountViewSet
+
+from django.conf.urls import include, patterns, url
+
+router = routers.SimpleRouter()
+router.register(r'account', AccountViewSet)
 
 urlpatterns = patterns(
     '',
+    # ... URLs
+    url(r'^api/v1/', include(router.urls)),
 
     url('^.*$', IndexView.as_view(), name='index'),
 )
